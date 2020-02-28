9bc330424c5e7fcdb99fc6b27d714efc58170c8d
183503 <michal.rusiecki@santander.pl>
2020-02-28 08:25:38 +0100

    full script

--- hello-world.py
+++ hello-world.py
@@ -10,13 +10,21 @@ def print_xml():
 
     with open('output_test.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=";")
+        writer.writerow(row_list)
         for testcase in root.iter('testcase'):
-            print("Name: " + testcase.attrib['name'])
+            steps_array = []
+            index = 0
+            testcase_name = testcase.attrib['name']
+            print("Name: " + testcase_name)
             for step in testcase.iter('step'):
                 final_action = step.find('actions').text
                 expected_result = step.find('expectedresults').text
                 print("Action:" + final_action)
                 print("Expected result: " + expected_result)
+                steps_array.append(prepare_execution_step(index, final_action, "", expected_result))
+                index += 1
+            writer.writerow([testcase_name, "183503", "183503", "10000", testcase_name, "Manual", str(steps_array)])
+            # print(steps_array)
             print("\n")
 
 
@@ -27,5 +35,5 @@ def prepare_execution_step(index, step, required_data, result):
 
 
 if __name__ == "__main__":
-    print(prepare_execution_step(0, "Hello World", "nothing", "expected result"))
-    # print_xml()
+    # print(prepare_execution_step(0, "Hello World", "nothing", "expected result"))
+    print_xml()
